53c4772fd79af047c0991a3f057c1bf490d4a67e
NantoYuma <naitoyuma@outlook.jp>
2020-06-08 01:08:18 +0900

    iOS13以降に対応 requestDeviceMotionPermission実装 httpsでないとセンサーアクセスできない。

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 
 let sensor_contents= document.getElementById("sensor_contents");
 let output = document.getElementById('output');
-let click = document.getElementById('click')
+let click = document.getElementById('click');
+let result1 = document.getElementById("result_acc");
+let result2 = document.getElementById("result_gyro");
 let number = 0;
 
 
@@ -17,23 +19,40 @@ const requestDeviceMotionPermission = function(){
     DeviceMotionEvent &&
     typeof DeviceMotionEvent.requestPermission === 'function'
   ) {
-    // iOS 13+ の Safari
+    // iOS 13+
     // 許可を取得
     DeviceMotionEvent.requestPermission()
     .then(permissionState => {
       if (permissionState === 'granted') {
         // 許可を得られた場合、devicemotionをイベントリスナーに追加
         window.addEventListener('devicemotion', e => {
-          aX = dat.accelerationIncludingGravity.x;
-          output.textContent = aX; 
-        })
+          var x = event.accelerationIncludingGravity.x;
+          var y = event.accelerationIncludingGravity.y;
+          var z = event.accelerationIncludingGravity.z;
+          
+          result1.innerHTML = "重力加速度<br />"+
+          "X：" + x.toFixed(2) +"(m/s^2)<br />" +
+          "Y：" + y.toFixed(2) +"(m/s^2)<br />" + 
+          "Z：" + z.toFixed(2) +"(m/s^2)<br />";
+        });
+
+        window.addEventListener( "deviceorientation", e => {
+          var alpha = event.alpha;
+          var beta = event.beta;
+          var gamma = event.gamma;
+    
+          result2.innerHTML = "ジャイロセンサー<br />" +
+            "alpha：" + alpha.toFixed(2) +"°<br />" +
+            "beta ：" + beta.toFixed(2)  +"°<br />" + 
+            "gamma：" + gamma.toFixed(2) +"°<br />";
+        }, false);
       } else {
-        // 許可を得られなかった場合の処理
+        output.textContent = "Not Accept";
       }
     })
     .catch(console.error) // https通信でない場合などで許可を取得できなかった場合
   } else {
-    output.textContent = "error";
+    output.textContent = "Error";
   }
 }
 
